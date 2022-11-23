30be1848805e7889b991b1e42707ae3b4644268a
annavishnyakova <vishnyakova0307@gmail.com>
2022-11-23 16:21:40 +0300

    негативный тест поиск доработка

--- conftest.py
+++ conftest.py
@@ -7,9 +7,10 @@ from selene import browser
 @pytest.fixture()
 def open_chome():
     browser.open_url('http://google.com/ncr')
+
+
+@pytest.fixture(open_chome)
+def browser_size():
     options = Options()
     options.add_argument('--window-size=1900,1000')
     return options
-
-
-
--- simple.test.py
+++ simple.test.py
@@ -6,9 +6,7 @@ def test_google_search(open_chome):
     browser.element('[name=q]').type('selene').press_enter()
     browser.element('[id=search]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
 
+
 def test_google_search_bad(open_chome):
     browser.element('[name=q]').type('hvkhgvbkubujgbhiljhbililhoiljh').press_enter()
     browser.element('[id=search]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
-
-
-
